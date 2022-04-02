25010d2a3d87531c39db77a28b814ec9758cac9e
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-03 00:23:52 +0300

    write test to save and delete user posts

--- instagram/tests.py
+++ instagram/tests.py
@@ -18,3 +18,24 @@ class TestProfile(TestCase):
         self.profile.save_profile()
         after = Profile.objects.all()
         self.assertTrue(len(after) > 0)
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile = Profile(name='brian', user=User(username='otieno'))
+        self.profile.save()
+
+        self.image = Post(image='brian.png', name='test', caption='test', user=self.profile)
+
+    def test_insatance(self):
+        self.assertTrue(isinstance(self.image, Post))
+
+    def test_save_image(self):
+        self.image.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
