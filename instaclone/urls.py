9299415b310e16528d7c51c7350cfa9452384b63
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-02 10:42:41 +0300

    test if the django framework is well set and is working

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path(r'^admin/', admin.site.urls),
+    path(r'', include('instagram.urls')),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    home = 'Welcome'
+    return render(request, 'home.html', {'home':home})
