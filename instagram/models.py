b98ac3c86c53daa99e386b834dfd71119fb163fa
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-02 10:58:06 +0300

    add the class methods for the model class profile

--- instagram/models.py
+++ instagram/models.py
@@ -13,3 +13,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
