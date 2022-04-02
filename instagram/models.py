2492dcca0d6a0e4476b0cd9b570be8524f519867
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-02 10:56:28 +0300

    add model class profile

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',)
+    profile_picture = models.ImageField(upload_to = 'images/')
+    name = models.CharField(blank=True, max_lenght = 120)
+    location = models.CharField(max_length=60, blank=True)
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
