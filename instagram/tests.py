f7b716f062bbe0e066fa5e6acecd3808fd45f249
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-03 00:22:01 +0300

    write test to save user

--- instagram/tests.py
+++ instagram/tests.py
@@ -5,5 +5,16 @@ from django.contrib.auth.models import User
 
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(username='charles')
+        self.user = User(username='brian')
         self.user.save()
+
+        self.profile = Profile(id=1, name='image', profile_picture='brian.jpg', bio='I am me',
+            user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_profile(self):
+        self.profile.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
